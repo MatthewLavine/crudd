f9d9aab3b4c5f3a5e227cbc3d4347810968889f4
Matthew Lavine <matt@matthewlavine.net>
2023-06-10 21:54:51 -0400

    Add service and install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -xe
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "Building CRUDD"
+
+go build .
+
+echo "Installing CRUDD"
+
+cp ./crudd /usr/bin/crudd
+
+echo "Installing CRUDD service"
+
+cp ./crudd.service /etc/systemd/system/crudd.service
+
+echo "Enabling CRUDD service"
+
+systemctl daemon-reload
+
+systemctl enable --now crudd.service
+
+echo "Done"
